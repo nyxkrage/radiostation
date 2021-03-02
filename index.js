0e7252647c1b18db75dcf05cb6551510ffa7695a
Carsten Kragelund <carsten@kragelund.dev>
2021-03-02 08:55:19 +0100

    Prettier and add more transitions

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const lru = require('lru_map')
-var path = require('path');
-var mpd = require('mpd'),
-	cmd = mpd.cmd;
+const lru = require("lru_map");
+var path = require("path");
+var mpd = require("mpd"),
+  cmd = mpd.cmd;
 var client = mpd.connect({
   port: 6600,
-  host: 'mpd',
+  host: "localhost",
 });
 
 let data = {
-	"num_clients" : 0,
-	"songs": new lru.LRUMap(10),
-}
+  num_clients: 0,
+  songs: new lru.LRUMap(10),
+};
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-var SSE = require('express-sse');
+var SSE = require("express-sse");
 var sse = new SSE();
 
-client.on('ready', function() {
-  client.sendCommand(cmd("currentsong", []), function(err, msg) {
+client.on("ready", function () {
+  client.sendCommand(cmd("currentsong", []), function (err, msg) {
     if (err) throw err;
     msg = mpd.parseKeyValueMessage(msg);
     data.songs.set(`${msg.Title} - ${msg.Artist}`, 0);
@@ -28,8 +28,8 @@ client.on('ready', function() {
   });
 });
 
-client.on('system-player', function() {
-  client.sendCommand(cmd("currentsong", []), function(err, msg) {
+client.on("system-player", function () {
+  client.sendCommand(cmd("currentsong", []), function (err, msg) {
     if (err) throw err;
     msg = mpd.parseKeyValueMessage(msg);
     data.songs.set(`${msg.Title} - ${msg.Artist}`, 0);
@@ -38,24 +38,22 @@ client.on('system-player', function() {
   });
 });
 
-
-app.get('/status', function(req, res) {
-	data.num_clients += 1;
-	req.on('close', () => {
-		data.num_clients -= 1;
-		sse.send(data.num_clients, "clients");
-	});
-	sse.init(req,res);
-	sse.send(data.songs, "songs");
-	sse.send(data.num_clients, "clients");
-	console.log(data);
+app.get("/status", function (req, res) {
+  data.num_clients += 1;
+  req.on("close", () => {
+    data.num_clients -= 1;
+    sse.send(data.num_clients, "clients");
+  });
+  sse.init(req, res);
+  sse.send(data.songs, "songs");
+  sse.send(data.num_clients, "clients");
+  console.log(data);
 });
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, 'static/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "static/index.html"));
 });
 
-app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, "static")));
 
-app.listen(8801, () => {
-});
+app.listen(8801, () => {});
